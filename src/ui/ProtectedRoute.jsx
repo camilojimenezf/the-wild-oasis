0909810d466402bb630f501a3018cbe12a891913
camilojimenezf <camilob270@gmail.com>
2023-10-30 16:52:57 -0300

    protecting routes with supabase auth

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useUser } from "../features/authentication/useUser";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+/* eslint-disable react/prop-types */
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { isLoading, isAuthenticated } = useUser();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return <div>{children}</div>;
+}
+
+export default ProtectedRoute;
